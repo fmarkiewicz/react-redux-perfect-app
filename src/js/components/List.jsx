44a04d85e796a42b5a0844c1eca0c01d494d7cba
Fryderyk Markiewicz <markiewiczfryderyk@gmail.com>
2019-03-13 16:18:27 +0100

    form component and redux actions

---
+++ src/js/components/List.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+const mapStateToProps = state => {
+    return { articles: state.articles };
+};
+
+const ConnectedList = ({ articles }) => (
+    <ul className="list-group list-group-flush">
+        {articles.map(el => (
+            <li className="list-group-item" key={el.id}>
+                {el.title}
+            </li>
+        ))}
+    </ul>
+);
+
+ConnectedList.propTypes = {
+    id: PropTypes.string,
+    title: PropTypes.string
+};
+
+const List = connect(mapStateToProps)(ConnectedList);
+
+export default List;
